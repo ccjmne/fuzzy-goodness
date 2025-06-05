708949fd1c037c917af9bfafb7bb670921c54a68
Éric NICOLAS <ccjmne@gmail.com>
2025-06-05 13:28:01 +0200

    One fewer pipe in the -line

--- find-contributors.sh
+++ find-contributors.sh
@@ -4,8 +4,7 @@
 export WIP=$(mktemp)
 export trailer=Co-authored-by
 git log --all --pretty='%an <%aE>%n%cn <%cE>%n%(trailers:key=Signed-off-by,valueonly)%n%(trailers:key=Acked-by,valueonly)%n%(trailers:key=Reviewed-by,valueonly)%n%(trailers:key=Helped-by,valueonly)%n%(trailers:key=Reported-by,valueonly)%n%(trailers:key=Co-authored-by,valueonly)' \
-    | grep . \
-    | awk '!M[$0]++' \
+    | awk '$0 && !M[$0]++' \
     | fzf --header 'Ctrl-R to cycle through trailers' \
         --multi \
         --preview 'cat $WIP' \
