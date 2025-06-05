99644875c7658a03f4fd695f7912e852f4ebc954
Éric NICOLAS <ccjmne@gmail.com>
2025-06-05 13:25:03 +0200

    Support adding multiple types of trailers at once

--- find-contributors.sh
+++ find-contributors.sh
@@ -1,15 +1,18 @@
 #! /usr/bin/env bash
 
 # TODO: Support amending HEAD
-# TODO: Support adding multiple types of trailers
-#       I mean adding some Co-authored-by and some Signed-off-by at once.
+export WIP=$(mktemp)
 export trailer=Co-authored-by
 git log --all --pretty='%an <%aE>%n%cn <%cE>%n%(trailers:key=Signed-off-by,valueonly)%n%(trailers:key=Acked-by,valueonly)%n%(trailers:key=Reviewed-by,valueonly)%n%(trailers:key=Helped-by,valueonly)%n%(trailers:key=Reported-by,valueonly)%n%(trailers:key=Co-authored-by,valueonly)' \
     | grep . \
     | awk '!M[$0]++' \
     | fzf --header 'Ctrl-R to cycle through trailers' \
         --multi \
-        --preview 'printf "%s\n" {+} | sed "s/^/${FZF_PROMPT%> }: /"' \
+        --preview 'cat $WIP' \
+        --bind 'ctrl-y:transform:
+            t=$(printf "--trailer='\''${FZF_PROMPT%> }: %s'\'' " {+})
+            sh -c "git interpret-trailers --if-exists addIfDifferent --in-place $t $WIP"
+            echo "deselect-all+refresh-preview"' \
         --bind 'start,ctrl-r:transform:
             case $FZF_PROMPT in
                 Signed-off-by*)  trailer=Reviewed-by    ;;
@@ -21,4 +24,4 @@ git log --all --pretty='%an <%aE>%n%cn <%cE>%n%(trailers:key=Signed-off-by,value
                 Co-authored-by*) trailer=Signed-off-by  ;;
             esac
             echo "change-prompt($trailer> )+refresh-preview"' \
-        --bind 'enter:become(git commit --template <(printf "\n\n"; printf "%s\n" {+} | sed "s/^/${FZF_PROMPT%> }: /"))'
+        --bind 'enter:become(git commit --template <(printf "\n"; cat $WIP))'
