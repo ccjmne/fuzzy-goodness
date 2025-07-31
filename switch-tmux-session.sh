043526dd7791a4367fc621f19368dc5c1306c9ee
Éric NICOLAS <ccjmne@gmail.com>
2025-07-31 12:46:09 +0200

    Handle switch-tmux-session invocation inside popup

--- switch-tmux-session.sh
+++ switch-tmux-session.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
 # TODO: Default to last session, suffix with '-'
-session=$(tmux list-sessions -F '#{session_name}#{?session_attached,*,}' | grep -v '*$' \
-  | fzf --tmux border-native --info hidden --header "$(tmux display-message -p '#S*')")
+if [ -n "$TMUX_POPUP" ]; then
+  session=$(tmux list-sessions -F '#{session_name}#{?session_attached,*,}' | grep -v '*$' \
+    | fzf                      --info hidden --header "$(tmux display-message -p '#S*')")
+else
+  session=$(tmux list-sessions -F '#{session_name}#{?session_attached,*,}' | grep -v '*$' \
+    | fzf --tmux border-native --info hidden --header "$(tmux display-message -p '#S*')")
+fi
 
 if [ -n "$session" ]; then
   tmux switch-client -t "$session" 2>/dev/null || tmux attach-session -t "$session"
