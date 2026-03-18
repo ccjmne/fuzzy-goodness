3bf05961468cf562dcf8e63d446c272d5e27698e
Éric NICOLAS <ccjmne@gmail.com>
2026-03-18 14:51:25 +0100

    Support spaces in switch-tmux-session session name

--- switch-tmux-session.sh
+++ switch-tmux-session.sh
@@ -5,4 +5,4 @@ P=$(tmux display -p '#{client_last_session}')
 tmux list-sessions -F '#S' | sed -n "/^${P:-:}$\|^${S}$/!H;/^$P$/"'{s/$/:-/p};${g;s/\n//p}' \
   | fzf $([ -z $TMUX_POPUP ] && echo --tmux border-native) --info hidden --header "$S*"     \
         --delimiter : --accept-nth 1 --with-nth {1}{2} --print-query | tail -1              \
-  | ifne xargs -II sh -c 'tmux new-session -ds I 2>/dev/null; tmux switch-client -t I'
+  | ifne xargs -II sh -c 'tmux new-session -ds "I" 2>/dev/null; tmux switch-client -t "I"'
