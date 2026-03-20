1b6effe3d72f2c65de7cd675f891fa0e889fa13b
Éric NICOLAS <ccjmne@gmail.com>
2026-03-20 01:50:28 +0100

    Show *next* C-R option in find-contributors header

--- find-contributors.sh
+++ find-contributors.sh
@@ -8,7 +8,6 @@ src=(Signed-off-by Reviewed-by Acked-by Tested-by Reported-by Suggested-by Co-de
 git log --all --pretty="%an <%aE>%n%cn <%cE>$(printf '\n%%(trailers:key=%s,valueonly)' ${src[@]})" \
     | awk '$0 && !M[tolower($0)]++' \
     | fzf \
-        --header 'Ctrl-R to cycle through trailers, Ctrl-Y to validate selection' \
         --multi \
         --preview '
             t=$(printf "--trailer='\''${FZF_PROMPT%> }: %s'\'' " {+})
@@ -19,15 +18,15 @@ git log --all --pretty="%an <%aE>%n%cn <%cE>$(printf '\n%%(trailers:key=%s,value
             echo "clear-multi+clear-query+first+refresh-preview"' \
         --bind 'start,ctrl-r:transform:
             case $FZF_PROMPT in
-                Signed-off-by*)  trailer=Reviewed-by    ;;
-                Reviewed-by*)    trailer=Acked-by       ;;
-                Acked-by*)       trailer=Tested-by      ;;
-                Tested-by*)      trailer=Reported-by    ;;
-                Reported-by*)    trailer=Suggested-by   ;;
-                Suggested-by*)   trailer=Co-authored-by ;;
-                Co-authored-by*) trailer=Signed-off-by  ;;
+                Signed-off-by*) trailer=Reviewed-by    next=Acked-by       ;;
+                Reviewed-by*)   trailer=Acked-by       next=Tested-by      ;;
+                Acked-by*)      trailer=Tested-by      next=Reported-by    ;;
+                Tested-by*)     trailer=Reported-by    next=Suggested-by   ;;
+                Reported-by*)   trailer=Suggested-by   next=Co-authored-by ;;
+                Suggested-by*)  trailer=Co-authored-by next=Signed-off-by  ;;
+                *)              trailer=Signed-off-by  next=Reviewed-by    ;;
             esac
-            echo "change-prompt($trailer> )+refresh-preview"' \
+            echo "change-header(Ctrl-R: $next, Ctrl-Y: apply current)+change-prompt($trailer> )+refresh-preview"' \
         --bind 'enter:become:
             t=$(printf "--trailer='\''${FZF_PROMPT%> }: %s'\'' " {+})
             sh -c "git interpret-trailers --if-exists addIfDifferent $t $WIP --in-place"
