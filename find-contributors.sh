8ec9709b06730e2eb396434b9120c7fb78d2bfb6
Éric NICOLAS <ccjmne@gmail.com>
2026-04-21 22:42:47 +0200

    Polish find-contributors, drop trailer var. export

--- find-contributors.sh
+++ find-contributors.sh
@@ -3,19 +3,18 @@
 # TODO: Support amending HEAD
 # TODO: Filter out contributors already anchored w/ current trailer
 export WIP=$(mktemp)
-export trailer=Co-authored-by
 src=(Signed-off-by Reviewed-by Acked-by Tested-by Reported-by Suggested-by Co-developed-by Co-authored-by)
-git log --all --pretty="%aN <%aE>%n%cN <%cE>$(printf '\n%%(trailers:key=%s,valueonly)' ${src[@]})" \
-    | awk '$0 && !M[tolower($0)]++' \
-    | fzf \
-        --multi \
+git log --all --pretty="%aN <%aE>%n%cN <%cE>$(printf '\n%%(trailers:key=%s,valueonly)' ${src[@]})"                  \
+    | awk '$0 && !M[tolower($0)]++'                                                                                 \
+    | fzf                                                                                                           \
+        --multi                                                                                                     \
         --preview '
             t=$(printf "--trailer='\''${FZF_PROMPT%> }: %s'\'' " {+})
             sh -c "git interpret-trailers --if-exists addIfDifferent $t $WIP" | bat --language=yaml --color=always' \
         --bind 'ctrl-y:transform:
             t=$(printf "--trailer='\''${FZF_PROMPT%> }: %s'\'' " {+})
             sh -c "git interpret-trailers --if-exists addIfDifferent $t $WIP --in-place"
-            echo "clear-multi+clear-query+first+refresh-preview"' \
+            echo "clear-multi+clear-query+first+refresh-preview"'                                                   \
         --bind 'start,ctrl-r:transform:
             case $FZF_PROMPT in
                 Signed-off-by*) trailer=Reviewed-by    next=Acked-by       ;;
@@ -26,7 +25,7 @@ git log --all --pretty="%aN <%aE>%n%cN <%cE>$(printf '\n%%(trailers:key=%s,value
                 Suggested-by*)  trailer=Co-authored-by next=Signed-off-by  ;;
                 *)              trailer=Signed-off-by  next=Reviewed-by    ;;
             esac
-            echo "change-header(Ctrl-R: $next, Ctrl-Y: apply current)+change-prompt($trailer> )+refresh-preview"' \
+            echo "change-header(Ctrl-R: $next, Ctrl-Y: apply current)+change-prompt($trailer> )+refresh-preview"'   \
         --bind 'enter:become:
             t=$(printf "--trailer='\''${FZF_PROMPT%> }: %s'\'' " {+})
             sh -c "git interpret-trailers --if-exists addIfDifferent $t $WIP --in-place"
